cc049feceefb7a6b5fb0cd910f5abd588a6d3815
muratcan <muratcan_yeldan@hotmail.com>
2021-03-18 11:41:42 +0300

    Geliştirmeler tamamlandı. Test üzerinde çalışmalar devam ediyor

--- app/src/main/java/com/muratcanapps/mvvm_login_screen/model/SignInWithEmailRequest.kt
+++ app/src/main/java/com/muratcanapps/mvvm_login_screen/model/SignInWithEmailRequest.kt
@@ -3,12 +3,14 @@ package com.muratcanapps.mvvm_login_screen.model
 import com.google.gson.annotations.SerializedName
 
 data class SignInWithEmailRequest(
+    
     /*
     Alternative = " ... " kullanılabiliyor
     Bu sayede field'ın birden fazla ismi olabiliyorsa
     username , email gibi ikisinin aynı değişkene
     atılması sağlanabiliyor.
      */
+
     @SerializedName("email")
     var email: String,
 
